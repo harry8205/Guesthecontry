3a9d06272e2bbe6935060ac8175cf837934c2206
Haris Ali <aliharis601@gmail.com>
2023-02-18 23:50:35 +0500

    signin

--- app/src/main/java/com/example/guessthecountry/SigningAreaActivity.kt
+++ app/src/main/java/com/example/guessthecountry/SigningAreaActivity.kt
@@ -7,5 +7,6 @@ class SigningAreaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signing_area)
+
     }
 }
